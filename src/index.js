f5e4608bf2e3146720c8ccf42f22ef13702cca10
Ali Goergen <alyssa.goergen@gmail.com>
2021-10-27 14:12:17 -0400

    note app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+const NoteApp = () => {
+  const notesData = JSON.parse(localStorage.getItem('notes'))
+  const [notes, setNotes] = useState(notesData || [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([...notes, { title, body }])
+    setTitle('')
+    setBody('')
+  }
+  const removeNote = (title) => {
+    setNotes(notes.filter((note)=> note.title !== title))
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('notes', JSON.stringify(notes))
+  })
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note)=> (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={()=> {removeNote(note.title)}}>X</button>
+        </div>
+      ))}
+      <p>add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e)=> setTitle(e.target.value)}/>
+        <div>
+          <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        </div>
+        <button>add note</button>
+
+      </form>
+    </div>
+  )
+}
+
+// const App = () => {
+//   const [count] = useState(10)
+//   //2 things
+//   const increment = () => {
+    
+//   }
+//   return (
+//     <div>
+//       <p>the current count is {count}</p>
+//       <button onClick={increment}>+1</button>
+//     </div>
+//   )
+// }
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <NoteApp/>,
   document.getElementById('root')
 );
 
